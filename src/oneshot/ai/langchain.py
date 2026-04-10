91db833f58d004c80551ef31a40c287ee286c216
Fritz Duchardt <fritz@duchardt.net>
2026-04-10 17:59:56 +0200

    Added token count validation

--- src/oneshot/ai/langchain.py
+++ src/oneshot/ai/langchain.py
@@ -3,8 +3,6 @@ import logging
 import os
 import warnings
 
-from .anthropic_utils import MAX_TOKENS
-
 warnings.filterwarnings(
     "ignore",
     message=r".*Core Pydantic V1 functionality isn't compatible with Python 3\.14 or greater.*",
@@ -18,8 +16,10 @@ from langchain.chat_models import init_chat_model, BaseChatModel
 from langchain_google_genai import ChatGoogleGenerativeAI
 from . import ai_utils
 
-TOKEN_LIMIT_MCP = 20000
-TOKEN_LIMIT_CLI = 10
+MAX_INPUT_TOKENS_MCP = 20000
+MAX_INPUT_TOKENS_CLI = 200000
+MAX_OUTPUT_TOKENS_MCP = 20000
+MAX_OUTPUT_TOKENS_CLI = -1
 
 client = MultiServerMCPClient(
     {
@@ -37,10 +37,10 @@ client = MultiServerMCPClient(
 
 def call_ai(model: str, pattern: str, prompt: str) -> str:
     logging.info("Calling AI without tools")
-    llm = _create_llm(model, TOKEN_LIMIT_CLI)
+    llm = _create_llm(model, MAX_OUTPUT_TOKENS_CLI)
     messages = _create_messages(pattern, prompt)
 
-    _validate_token_count(llm, messages, TOKEN_LIMIT_CLI)
+    _validate_token_count(llm, messages, MAX_INPUT_TOKENS_CLI)
 
     response = llm.invoke(messages)
     logging.info(f"Input tokens: {response.usage_metadata['input_tokens']}")
@@ -51,10 +51,10 @@ def call_ai(model: str, pattern: str, prompt: str) -> str:
 async def call_ai_with_tools(model: str, pattern: str, prompt: str) -> str:
     try:
         available_tools = await client.get_tools()
-        llm = _create_llm(model, TOKEN_LIMIT_MCP)
+        llm = _create_llm(model, MAX_OUTPUT_TOKENS_MCP)
         messages = _create_messages(pattern, prompt)
 
-        _validate_token_count(llm, messages, TOKEN_LIMIT_MCP)
+        _validate_token_count(llm, messages, MAX_INPUT_TOKENS_MCP)
 
         agent = create_agent(
             model=llm,
@@ -75,7 +75,7 @@ async def call_ai_with_tools(model: str, pattern: str, prompt: str) -> str:
         return "Failed on call to MCP Server and / or LLM. Check logs"
 
 
-def _validate_token_count(llm, messages, token_count=MAX_TOKENS) -> None:
+def _validate_token_count(llm, messages, token_count) -> None:
     estimated_tokens = None
 
     if hasattr(llm, "get_num_tokens_from_messages"):
@@ -93,7 +93,7 @@ def _validate_token_count(llm, messages, token_count=MAX_TOKENS) -> None:
         )
 
 
-def _create_llm(model: str, max_tokens: int) -> BaseChatModel:
+def _create_llm(model: str, max_output_tokens: int) -> BaseChatModel:
     ret: BaseChatModel
     if model.startswith("gemini"):
         ret = ChatGoogleGenerativeAI(
@@ -103,7 +103,8 @@ def _create_llm(model: str, max_tokens: int) -> BaseChatModel:
         )
     else:
         ret = init_chat_model(model)
-    ret.max_tokens = max_tokens
+    if max_output_tokens > 0:
+        ret.max_tokens = max_output_tokens
     return ret
 
 
